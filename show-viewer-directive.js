5bb4dc8d5172f34ce25ebcf53ca1145051daa01d
Mark K Cowan <mark@battlesnake.co.uk>
2015-01-13 19:46:24 +0200

    Show-viewer totally decoupled from timeline (white arrow is in neither)

--- show-viewer-directive.js
+++ show-viewer-directive.js
@@ -4,26 +4,14 @@
 	angular.module('battlesnake.show-viewer')
 		.directive('showViewer', showViewerDirective);
 
-	function showViewerDirective($window) {
+	function showViewerDirective() {
 		return {
 			restrict: 'A',
 			scope: {
 				show: '=showViewer',
-				collapsed: '@collapsed',
-				collapsible: '@collapsible'
 			},
-			templateUrl: '../show-viewer-template.html',
-			link: link
+			templateUrl: '../show-viewer-template.html'
 		};
-
-		function link(scope, element, attrs) {
-			scope.$watch('show', showChanged);
-			return;
-
-			function showChanged() {
-				scope.collapsed = false;
-			}
-		}
 	}
 
 })(window.angular);
