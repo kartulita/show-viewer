707ca3877af56db66d34b0934ccb75d4a05f6f8f
Mark K Cowan <mark@battlesnake.co.uk>
2015-01-14 20:06:37 +0200

    Demos are now awesome, media plays

--- demos/etv-adapter.js
+++ demos/etv-adapter.js
@@ -20,15 +20,19 @@
 	 * For chrome/chromium: --disable-web-security
 	 */
 	angular.module('demo')
-		.factory('ETVTimelineAdapter', ETVTimelineAdapter);
+		.value('languageId', 0)
+		.factory('ETVTimelineAdapter', ETVTimelineAdapter)
+		;
 
-	function ETVTimelineAdapter() {
-
+	function ETVTimelineAdapter($http, $sce, languageId) {
+		var endpoint = 'http://etv.err.ee/api/loader';
 		return {
 			/* URL to get timeline items per day */
-			endpoint: 'http://etv.err.ee/api/loader/GetTimelineDay',
-			/* Applied to each retrieved item to map it to the view's format */
-			mapItem: mapItem
+			endpoint: endpoint + '/GetTimelineDay',
+			/* Applied to each retrieved timeline item to map it to the view's format */
+			mapItem: mapItem,
+			/* Used to get info about an item for use by the show viewer */
+			getDetails: getDetails
 		};
 
 		function mapItem(item) {
@@ -56,6 +60,96 @@
 			function getImageUrl(item) {
 				return 'http://static.err.ee/gridfs/' + item.Image +
 					'?width=600&height=600&mode=crop&quality=75';
+			}
+		}
+
+		/* Map show item (backend format) to show-viewer format */
+		function getDetails(item) {
+			if (item.Id.match(/^[0\-]+$/)) {
+				return new ShowData(item);
+			} else {
+				return $http.get(endpoint + '/GetTimelineContent/' + item.Id)
+					.then(function (response) {
+						return new ShowData(item, response.data);
+					});
+			}
+
+			function ShowData(baseData, contentData) {
+				contentData = contentData || {};
+				this.baseData = baseData;
+				this.contentData = contentData;
+				this.title = getText(contentData.Texts, baseData.HeaderLong || baseData.Header, 'Header');
+				this.image = getImage(contentData.Image || baseData.Image);
+				this.description = $sce.trustAsHtml(getText(contentData.Texts, baseData.Lead, 'Lead'));
+				this.start = moment(contentData.Published || contentData.Created || baseData.Published),
+				/* TODO: GetCategoryUrlForPrimaryCategory */
+				this.categoryUrl = contentData.PrimaryCategoryId;
+				this.targetUrl = contentData.Url;
+				/* TODO: channelUrl */
+				this.channelurl = '';
+				if (contentData.disableList) {
+					this.media = null;
+				} else {
+					var sources = _(contentData.MediaSources || []).chain()
+						.filter(function (source) { return !!source; })
+						.map(JSON.stringify.bind(JSON))
+						.uniq()
+						.map(JSON.parse.bind(JSON))
+						.value();
+					this.media = sources.length ? new MediaList(this, sources, contentData) : null;
+				}
+			}
+
+			function getImage(image) {
+				if (typeof image === 'string') {
+					return image;
+				} else if (typeof image === 'object') {
+					return 'http://static.err.ee/gridfs/' + image.FileName + (image.ImageResizerOptions || '');
+				} else {
+					return null;
+				}
+			}
+
+			function getText(texts, fallback, key) {
+				var text;
+				if (texts && texts.length) {
+					text = _(texts).findWhere({ Language: languageId });
+					if (!text) {
+						text = texts[0];
+					}
+					return text[key];
+				} else {
+					return fallback;
+				}
+			}
+
+			function MediaList(show, sources, data) {
+				this.sources = _(sources).chain().map(mapSource).unique().value();
+				this.select = selectSource;
+				this.select(0);
+				return;
+			
+				function mapSource(source) {
+					var parts = source.Content.replace('://', '').split('@');
+					var url = 'http://static.err.ee/media?' +
+						[
+							'stream=' + parts[0],
+							'file=' + parts[1],
+							'mediaspace=mediaframe',
+							'autoplay=true',
+							'mediamode=wowzavideo',
+							'width=100',
+							/* 'site=' + window.location.host, */
+							'image=' + show.image + '?width=752'
+						].join('&');
+					/* console.log(_.object(url.split('&').map(function (s) { return s.split('='); }))); */
+					return $sce.trustAsResourceUrl(url);
+				}
+
+				function selectSource(index) {
+					this.sourceIndex = index;
+					this.source = this.sources[index];
+				}
 			}
 		}
 	}
--- show-viewer-directive.js
+++ show-viewer-directive.js
@@ -10,8 +10,28 @@
 			scope: {
 				show: '=showViewer',
 			},
-			templateUrl: '../show-viewer-template.html'
+			templateUrl: '../show-viewer-template.html',
+			link: link
 		};
+
+		function link(scope, element, attrs) {
+			scope.selectSource = selectSource;
+			scope.$watch('show', showChanged);
+			return;
+
+			function showChanged() {
+				var show = scope.show;
+				if (show && show.media && show.media.sources && show.media.sources.length) {
+					selectSource(show.media.sources[0]);
+				} else {
+					scope.activeSource = null;
+				}
+			}
+
+			function selectSource(source) {
+				scope.activeSource = source;
+			}
+		}
 	}
 
 })(window.angular);
